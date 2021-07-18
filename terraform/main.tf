0582f95b6a3b4f573dd8b10f18d6900b7fa8cde7
mlgallego <84006917+mlgallego@users.noreply.github.com>
2021-07-18 16:08:15 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,7 @@
-# El bloque required_providers establece el origen (source) y la version  a emplear
+											      [modify]
+
+
+# El bloque required_providers establece el origen (source) y la version  a emplear [1]
 terraform {
   required_providers {
     azurerm = {
@@ -8,8 +11,8 @@ terraform {
   }
 }
 
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 
+# Administración de un grupo de recursos [2]
 resource "azurerm_resource_group" "rg" {
     name     = "kubernetes_rg"
     location = var.location
@@ -20,18 +23,25 @@ resource "azurerm_resource_group" "rg" {
 
 }
 
-# Storage account
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
 
+# Creación de cuenta de almacenamiento de Azure [3]
 resource "azurerm_storage_account" "storageAccount" {
-    name                     = "accstor78452126" 
+    name                     = "accstor78452126"      # Nombre de la cuenta de almacenamiento.
+    							  Debe ser único en todo Azure,
+                                                        no solo dentro del grupo de recursos. 
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+    account_tier             = "Standard"             # Nivel de la cuenta de almacenamiento
+    account_replication_type = "LRS"                  # Tipo de replicación
 
     tags = {
         environment = "CP2"
     }
 
 }
+
+
+# Enlace a los recursos:
+# [1]https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+# [2]https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+# [3]https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
